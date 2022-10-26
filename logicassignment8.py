b22fba9616145ab0bebab68bf040343ce688cd9d
Frxstz <116678374+Frxstz@users.noreply.github.com>
2022-10-26 08:55:01 -0500

    Update logicassignment8.py

--- logicassignment8.py
+++ logicassignment8.py
@@ -1,18 +1,23 @@
-"""intro2ProgramingLogic_assigment7"""
-#daniel_Gauthier
-#oct_2022
+"""
+Logic assigment 8
+Author @Frxstz
+https://github.com/Frxstz/ProLogic/blob/main/logicassignment8.py
+Oct_2022
+Funtions Assignment
+"""
 class Student:
     X = 0
     while True:
         USER = str(input("Do you want to pay for a student? [Yes/No] ")).lower()
         if USER == "yes":
-            X = X + 1
-            #general_data_collection
-            print ("Hello, welcome to the Red River Collge Student services")
+            X = X + 1   #Counter of how many students you registered
+            print ("Hello, welcome to the College Student services")
             FULL_NAME = str(input("What is your full name? "))
             number_credits = int(input("How many credits are you taking this term? "))
 
-            def extra_cost(DORMS_PAYMENT, FOOD_PAYMENT):
+            def extra_cost():       #This works i guess, can be optimised
+                FOOD_PAYMENT = (int(0))
+                DORMS_PAYMENT = (int(0))
                 DORMS = str(input("Do you plan to get an dormitory on campus? [Yes/No] ")).lower()
                 if DORMS == ("yes"):
                     DORMS_PAYMENT = (int(1500))
@@ -21,14 +26,14 @@ class Student:
                         FOOD_PAYMENT = (int(800))
                     else :FOOD_PAYMENT = (int(0))
                 else :DORMS_PAYMENT = (int(0))
-                print(f"{DORMS_PAYMENT}")
-
-            DORMS_PAYMENT = (int(0))
-            FOOD_PAYMENT = (int(0))
-            extra_cost(DORMS_PAYMENT, FOOD_PAYMENT)
+                addon = DORMS_PAYMENT + FOOD_PAYMENT
+                return addon
+            Final_cost = extra_cost()   #Taking extra_cost and turning it into an Int(final_Cost)
+            print(f"{Final_cost}")
+            #Taking the Final_cost and turning it into Final_payment
             payment = number_credits*300
-            final_payment = payment + DORMS_PAYMENT + FOOD_PAYMENT
+            final_payment = payment + Final_cost    #Final Value
             print (f"{FULL_NAME}, For this term you owe us {final_payment}$")
-        else:     
-            print(f"You paid for {X} Student's")
-            break
+        else:
+            print(f"You paid for {X} Student's")    #How many students you paid for 
+            break                                   #Break While loop
